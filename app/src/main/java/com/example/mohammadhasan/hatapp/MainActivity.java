1be49782df9dc2a8f42ffa2ad3836833d7be9fcf
Hatman42 <mashroorhasan2@gmail.com>
2017-11-19 13:38:40 -0500

    I tried to add a login but I didnt want to

--- app/src/main/java/com/example/mohammadhasan/hatapp/MainActivity.java
+++ app/src/main/java/com/example/mohammadhasan/hatapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.mohammadhasan.hatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
