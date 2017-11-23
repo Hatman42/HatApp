f4fe411dfe66c1e499a3e1b37ab21a1d9e59f1f8
Hatman42 <mashroorhasan2@gmail.com>
2017-11-23 12:22:16 -0500

    added a github repo button that leads to this repo

--- app/src/main/java/com/example/mohammadhasan/hatapp/MainActivity.java
+++ app/src/main/java/com/example/mohammadhasan/hatapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mohammadhasan.hatapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,4 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+public void browser1(View view){
+    Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/Hatman42/HatApp"));
+    startActivity(browserIntent);
+}
 }
